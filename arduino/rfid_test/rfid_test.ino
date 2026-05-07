1784e6b324bcbe0b258869fd36f63f2fb5c3db07
allFromStars <xia.yunhe@outlook.com>
2026-05-07 10:40:12 +0100

    RFID working code

---
+++ arduino/rfid_test/rfid_test.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <MFRC522_I2C.h>
+
+#define RFID_ADDR 0x28
+
+MFRC522_I2C rfid(RFID_ADDR, -1);
+
+bool startupMessagePrinted = false;
+
+
+void setup() {
+
+  Serial.begin(115200);
+
+  while (!Serial) {
+    delay(10);
+  }
+
+  delay(1000);   // Give Serial Monitor time to fully open
+
+  Wire.begin();  // GIGA R1 I2C
+  delay(500);
+
+  rfid.PCD_Init();
+  delay(100);
+
+  Serial.println();
+  Serial.println("WS1850S RFID reader ready");
+  Serial.println("Place card near reader...");
+  Serial.println();
+
+  startupMessagePrinted = true;
+}
+
+
+void loop() {
+
+  if (!startupMessagePrinted) {
+    Serial.println("WS1850S RFID reader ready");
+    Serial.println("Place card near reader...");
+    startupMessagePrinted = true;
+  }
+
+  if (!rfid.PICC_IsNewCardPresent()) {
+    delay(100);
+    return;
+  }
+
+  if (!rfid.PICC_ReadCardSerial()) {
+    delay(100);
+    return;
+  }
+
+  Serial.print("UID: ");
+
+  for (byte i = 0; i < rfid.uid.size; i++) {
+
+    if (rfid.uid.uidByte[i] < 0x10) {
+      Serial.print("0");
+    }
+
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    Serial.print(" ");
+  }
+
+  Serial.println();
+
+  delay(1000);
+}
