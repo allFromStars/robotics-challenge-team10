ea4c52548c40dc7c4b3aa2ac3ee3273d21267704
allFromStars <xia.yunhe@outlook.com>
2026-05-13 11:49:26 +0100

    update pinouts

--- arduino/robot_demo/ir_sensor_test/ir_sensor_test.ino
+++ arduino/robot_demo/ir_sensor_test/ir_sensor_test.ino
@@ -1,7 +1,7 @@
 const uint8_t SensorCount = 9;
 
 const uint8_t sensorPins[SensorCount] = {
-  2, 3, 4, 5, 6, 7, 8, 9, 10
+  22, 23, 24, 25, 26, 27, 28, 29, 30
 };
 
 uint16_t sensorValues[SensorCount];
