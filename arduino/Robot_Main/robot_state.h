aebf13d960234991ac3bb53a646457a2ba36cb24
allFromStars <xia.yunhe@outlook.com>
2026-05-28 11:56:51 +0100

    add headers

---
+++ arduino/Robot_Main/robot_state.h
@@ -0,0 +1,19 @@
+#ifndef ROBOT_STATE_H
+#define ROBOT_STATE_H
+
+enum RobotState {
+  STATE_STANDBY_BASE,       // Parked at base waiting for start cue
+  STATE_LINE_FOLLOWING_B,     // Tracking line in base
+  STATE_PLAN, 
+  STATE_NAVIGATING_LINES,     // Tracking line in arena
+  STATE_NAVIGATING_OPEN,      // Navigating in open field
+  STATE_WALL_FOLLOWING,     // Wall following in Arena
+  STATE_ALIGN_SEED,         // After wanted RFID detected, creep forward and use reflectance to align itself slowly
+  STATE_PLANTING_CYCLE,     // Paused over an RFID seed hole, activating the hopper mechanism
+  STATE_RESCUE_MODE,        // received signal to save robot that is right infront, tap robot
+  STATE_STRANDED_ALIVE,     // Stop wheels activate LED, do stranded protocol
+  STATE_REVIVED_RETURN,     // Revived - do revival protocol
+  STATE_EMERGENCY_STOP      // Hardware kill switch fallback state
+};
+
+#endif
